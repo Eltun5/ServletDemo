fd81602df56ee1f3de2d6847f6ec3e47756554ad
Eltun <aslanoveltun28@gmail.com>
2024-11-14 23:56:46 +0400

    Create project and add helloServlet

---
+++ src/main/java/com/EltunAslanov/servlet/HelloServlet.java
@@ -0,0 +1,21 @@
+package com.EltunAslanov.servlet;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class HelloServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html");
+        PrintWriter writer = resp.getWriter();
+        writer.write("<h1>Hello Servlet</h1>");
+        writer.close();
+    }
+
+}
